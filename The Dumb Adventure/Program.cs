5dce75cf919f5ba1bd87549e1db88465a0d087fe
Magnusvesthan <magnusvesthan@gmail.com>
2026-08-21 11:04:56 +0200

    fix

--- The Dumb Adventure/Program.cs
+++ The Dumb Adventure/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- The Dumb Adventure/wwwroot/JS/script.js
+++ The Dumb Adventure/wwwroot/JS/script.js
@@ -2,22 +2,22 @@ let points = 0;
 document.getElementById("newEvent").addEventListener("click", loadEvent);
 
 function loadEvent() {
-    fetch("http://localhost:5500/api/event")
+    fetch("http://localhost:5250/api/event")
         .then(res => res.json())
         .then(data => {
-            document.getElementById("scenario").innerText = data.Scenario;
+            document.getElementById("scenario").innerText = data.scenario;
             document.getElementById("result").innerText = "";
             const optionDiv = document.getElementById("options");
             optionDiv.innerText = "";
 
-            data.Options.forEach(option => {
+            data.options.forEach(option => {
                 const btn = document.createElement("button");
                 btn.className = "option-btn";
-                btn.innerText = option.Text;
+                btn.innerText = option.text;
 
                 btn.onclick = () => {
-                    points += option.Points
-                    document.getElementById("result").innerText = option.Result;
+                    points += option.points
+                    document.getElementById("result").innerText = option.result;
                     document.getElementById("points").innerText = "Point " + points;
                 };
                 optionDiv.appendChild(btn);
